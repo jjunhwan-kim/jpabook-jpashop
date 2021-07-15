844850f0a9cea8b538261b028ffa216493486a8a
nefertirii <nefertiri972@gmail.com>
2021-07-15 23:40:47 +0900

    Add entity method
    - 연관관계 편의 메소드 추가(양방향 연관 관계일 경우)
    - fetch 속성 Lazy 로 설정(N + 1 문제 방지)

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -20,6 +20,8 @@ public class Member {
     @Embedded
     private Address address;
 
+    // 하이버네이트는 엔티티를 영속화 할 때 컬렉션을 감싸서 하이버네이트가 제공하는 내장 컬렉션으로 변경함
+    // 따라서 컬렉션을 변경하거나 잘못 생성하면 하이버네이트 내부 메커니즘에 문제가 발생할 수 있으므로 필드 레벨에서 생성하는 것이 안전함
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -17,14 +19,20 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    /*
+      fetch = FetchType.EAGER 설정시 JPQL 사용시
+      JPQL "select o from order o" 가 SQL로 그대로 변억되어 "select * from order" 로 나가게 됨
+      그런데 Eager로 설정되어 order 개수 n개 만큼 member SQL이 나가게 됨
+      따라서 결과적으로 N(member) + 1(order) 만큼 SQL이 나가게 되어 N + 1 문제라고 함
+    */
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -32,4 +40,19 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
