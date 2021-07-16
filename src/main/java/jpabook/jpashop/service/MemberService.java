0099356884c34a41e0f2c89bbc9759d6c62911b0
nefertirii <nefertiri972@gmail.com>
2021-07-16 22:33:21 +0900

    Add member service
    - 주입 받을 필드를 final로 선언하고, @RequiredArgsConstructor 를 클래스 레벨에 선언하여 생성자 자동 주입 가능
    - 트랜잭션 기능 @Transactional 선언, 읽기만 동작하는 경우 readOnly 옵션으로 성능 최적화 가능
    - Member 유효성 검사시 동시성 문제로 동일한 Member 가 가입될 수 있으므로 데이터베이스에 name 컬럼에 유니크 제약조건을 설정하는 것을 추천함

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor // final 필드만 가진 생성자 자동생성
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+/*
+    @Autowired // 생성자가 1개일 경우 @Autowired 생략 가능
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+*/
+    // 회원 가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        // Exception
+        List<Member> findMembers = memberRepository.findByName(member.getName()); // 데이터베이스에 name을 유니크 제약조건을 설정하는 것 추천, 동시에 저장될 수 있는 가능성이 있음
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    private List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
