3c337992d8aa8388b9f7134e12045794b851e37c
nefertirii <nefertiri972@gmail.com>
2021-07-19 19:23:31 +0900

    Add member list
    - 회원 목록 조회 화면 추가

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -39,5 +40,14 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        // API로 반환할 때는 엔티티를 반환하면 안됨
+        // 일반적으로 MemberForm 이나 DTO를 넘겨서 화면에 출력하는 것을 권장함
+        model.addAttribute("members", members);
+        return "/members/memberList";
     }
 }
