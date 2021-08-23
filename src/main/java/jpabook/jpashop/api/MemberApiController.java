9690c1a2a10b9b29c34ab029589121efbf07ead8
nefertirii <nefertiri972@gmail.com>
2021-08-23 22:41:09 +0900

    Add member update api
    - 회원 수정 API 추가
    - UpdateMemberRequest, UpdateMemberResponse DTO 추가
    - Service의 update 메소드 호출시 Member 반환하는 것은 권장하지 않음. 커맨드와 쿼리는 분리하는게 좋음

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -2,12 +2,11 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -45,6 +44,27 @@ public class MemberApiController {
 
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request) {
+
+        memberService.update(id, request.getName()); // update 메서드에서 Member를 반환하도록 할 수 있으나 권장하지 않음
+        Member foundMember = memberService.findOne(id); // 커맨드와 쿼리는 분리하는게 좋음
+        return new UpdateMemberResponse(foundMember.getId(), foundMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
     }
 
     @Data
