1bb9b44dfa6e86e48ea3255768a0d6ff6357f233
nefertirii <nefertiri972@gmail.com>
2021-08-26 01:45:21 +0900

    Add order collection list api
    - 컬렉션 페치 조인시 페이징 불가능한 문제 발생
    - 컬렉션 페치 조인시 일대다에서 다를 기준으로 데이터베이스 로우가 생성되는데 페이징은 일을 기준으로 하므로 문제 발생
    - 컬렉션 엔티티를 조회하면서 페이징을 하기 위한 방법
    - 먼저 다대일, 일대일 관계를 모두 페치 조인
    - 컬렉션은 지연 로딩으로 설정
    - 지연 로딩 성능 최적화를 위해 default_batch_fetch_size 옵션을 적용
    - default_batch_fetch_size 옵션을 적용하면 컬렉션이나 프록시 객체를 설정한 size 만큼 한 번에 IN 쿼리로 조회
    - 쿼리 호출 수가 1 + N + M -> 1 + 1 + 1 로 최적화
    - 페치 조인 방식과 비교해서 쿼리 호출 수가 약간 증가하지만, DB 데이터 전송량이 감소
    - 컬렉션 페치 조인은 페이징이 불가능 하지만 이 방법은 페이징이 가능
    - ToOne 관계는 페치 조인해도 페이징에 영향을 주지 않으므로 ToOne 관계는 페치조인으로 쿼리 수를 줄이고, 나머지는 hibernate.default_batch_fetch_size 로 최적화
    - default_batch_fetch_size 의 크기는 적당한 사이즈를 골라야 하는데, 100~1000 사이를 선택하는 것을 권장
    - 1000으로 잡으면 한번에 1000개를 DB에서 애플리케이션에 불러오므로 DB 에 순간 부하가 증가할 수 있음
    - 하지만 애플리케이션은 100이든 1000이든 결국 전체 데이터를 로딩해야 하므로 메모리 사용량이 같음
    - 1000으로 설정하는 것이 성능상 가장 좋지만, 결국 DB든 애플리케이션이든 순간 부하를 어디까지 견딜 수 있는지로 결정하면 됨

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -9,6 +9,7 @@ import jpabook.jpashop.repository.OrderSearch;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -41,7 +42,7 @@ public class OrderApiController {
     }
 
     /**
-     *
+     * SQL 11번 실행
      * Order SQL / 1개 / Order 2개 조회, N = 2
      * Member SQL / N개 /
      * Delivery SQL / N개 /
@@ -59,9 +60,38 @@ public class OrderApiController {
         return collect;
     }
 
+    /**
+     * Fetch Join으로 SQL 1번 실행
+     */
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3() {
         List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return collect;
+    }
+
+
+    /**
+     * V3.1 엔티티를 조회해서 DTO로 변환 페이징 고려
+     *
+     * Batch Size 적용 전 SQL 7번 실행 ( 1 + N + M ), 1 = Order, N = OrderItem, M = Item
+     * Order, Member, Delivery SQL / 1개 / Order 2개 조회, N = 2
+     * OrderItems SQL / N개 /
+     * Order 1번의 OrderItem 2개, Item SQL 2개 추가
+     * Order 2번의 OrderItem 2개, Item SQL 2개 추가
+     * Batch Size 적용 후 SQL 3번 실행, 1 = Order, 1 = OrderItem, 1 = Item
+     * ToOne 관계만 우선 모두 페치 조인으로 최적화
+     * 컬렉션 관계는 hibernate.default_batch_fetch_size, @BatchSize로 최적화
+     */
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit) {
+
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
         List<OrderDto> collect = orders.stream()
                 .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
